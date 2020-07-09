095113dca455e7dc3227393019956e11ad9a227f
Ethan Rogers <erogers6264@gmail.com>
2020-07-09 08:49:06 -0600

    Create working & efficient solution

--- merge_meetings.py
+++ merge_meetings.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 # Merge meetings
 
-def is_mergeable(meeting1, meeting2):
-	# Sort the two meetings by start time
-	meetings = sorted([meeting1, meeting2])
+def merge_meetings(meetings):
+	sorted_meetings = sorted(meetings)
+	merged = [sorted_meetings.pop(0)]
+	for meeting in sorted_meetings:
+		if merged[-1][1] >= meeting[0]:
+			new_start_time = merged[-1][0]
+			new_end_time = meeting[1]
+			merged.pop()
+			merged.append((new_start_time, new_end_time))
+		else:
+			merged.append(meeting)
+	return merged
 
-	# If the end time of the first meeting is later than or the same as the
-	# start time of the second meeting then they are mergeable:
-	if meetings[0][1] >= meetings[1][0]:
-		return True
-	else:
-		return False
-
-
-print("The meetings are mergeable: " + str(is_mergeable((2, 7), (4, 10))))
+# Should return [(0, 1), (3, 8), (9, 12)]
+print(merge_meetings([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]))
