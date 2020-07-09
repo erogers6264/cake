ae50590317b19e359aed5a5ae5f29a015e125f54
Ethan Rogers <erogers6264@gmail.com>
2020-07-09 08:52:18 -0600

    Add comments explaining algorithm

--- merge_meetings.py
+++ merge_meetings.py
@@ -2,14 +2,22 @@
 # Merge meetings
 
 def merge_meetings(meetings):
+	# Sort the meetings by start time
 	sorted_meetings = sorted(meetings)
+
+	# Add the first meeting to a stack
 	merged = [sorted_meetings.pop(0)]
+
+	# Begin iterating through the rest
 	for meeting in sorted_meetings:
+		# If the last meeting's end time is the same or later than
+		# the next meeting's start time, merge them
 		if merged[-1][1] >= meeting[0]:
 			new_start_time = merged[-1][0]
 			new_end_time = meeting[1]
 			merged.pop()
 			merged.append((new_start_time, new_end_time))
+		# Otherwise just add it to the stack unmerged
 		else:
 			merged.append(meeting)
 	return merged
